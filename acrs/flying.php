8b9711ca40cf2853cd1ecffb40089bbf987ac390
Douglas Lovell <doug@wbreeze.com>
2011-07-19 18:54:29 -0400

    indentation

--- acrs/flying.php
+++ acrs/flying.php
@@ -14,98 +14,98 @@ require_once("redirect.inc");
 
 function doFlyingInfo()
 {
-$wasUpdated = FALSE;
-$readRecord = TRUE;
-$corrMsg = '';
-$userID = $_SESSION['userID'];
-$email = $_SESSION['email'];
-$registrant = $_POST;
-$db_conn = false;
-$fail = dbConnect($db_conn);
-if ($fail != '')
-{
-   notifyError($fail, "register.php");
-   $corrMsg = "<it>Internal: failed access to contest database</it>";
-   $readRecord = FALSE;
-}
-else if (isset($_POST["submit"]) || isset($_POST["save"]))
-{
-   $readRecord = FALSE;
-   // begin form processing
-   $certNumber = crop($registrant["certNumber"], 16);
-   if (strlen($certNumber) == 0)
-   {
-       $corrMsg .= "<li>Provide a pilot certificate number.</li>";
-   }
-   $regID = crop($registrant["airplaneRegID"], 16);
-   if (strlen($regID) == 0)
-   {
-       $corrMsg .= "<li>Provide airplane registration ID.</li>";
-   }
-   $insCo = crop($registrant["insCompany"], 24);
-   if (strlen($insCo) == 0)
-   {
-       $corrMsg .= "<li>Provide name of insurance company.</li>";
-   }
-   if ($corrMsg == '')
-   {
-      // have valid data
-      $fail = updateFlying($db_conn, $registrant, $userID);
-      if ($fail == '')
-      {
-          $wasUpdated = TRUE;
-      } 
-      else
-      {
-          notifyError($fail, "register.php");
-          $corrMsg = "<it>Internal: failed data update.</it>";
-      }
-   }
-}
-if ($readRecord)
-{
-  // not POST
-  $fail = retrieveRegistrant($db_conn, $registrant, $userID);
+  $wasUpdated = FALSE;
+  $readRecord = TRUE;
+  $corrMsg = '';
+  $userID = $_SESSION['userID'];
+  $email = $_SESSION['email'];
+  $registrant = $_POST;
+  $db_conn = false;
+  $fail = dbConnect($db_conn);
   if ($fail != '')
   {
      notifyError($fail, "register.php");
-     $corrMsg ="<it>Internal: failed access to registration record of ".$userID."</it>";
+     $corrMsg = "<it>Internal: failed access to contest database</it>";
+     $readRecord = FALSE;
   }
-}
-dbClose($db_conn);
+  else if (isset($_POST["submit"]) || isset($_POST["save"]))
+  {
+     $readRecord = FALSE;
+     // begin form processing
+     $certNumber = crop($registrant["certNumber"], 16);
+     if (strlen($certNumber) == 0)
+     {
+         $corrMsg .= "<li>Provide a pilot certificate number.</li>";
+     }
+     $regID = crop($registrant["airplaneRegID"], 16);
+     if (strlen($regID) == 0)
+     {
+         $corrMsg .= "<li>Provide airplane registration ID.</li>";
+     }
+     $insCo = crop($registrant["insCompany"], 24);
+     if (strlen($insCo) == 0)
+     {
+         $corrMsg .= "<li>Provide name of insurance company.</li>";
+     }
+     if ($corrMsg == '')
+     {
+        // have valid data
+        $fail = updateFlying($db_conn, $registrant, $userID);
+        if ($fail == '')
+        {
+            $wasUpdated = TRUE;
+        } 
+        else
+        {
+            notifyError($fail, "register.php");
+            $corrMsg = "<it>Internal: failed data update.</it>";
+        }
+     }
+  }
+  if ($readRecord)
+  {
+    // not POST
+    $fail = retrieveRegistrant($db_conn, $registrant, $userID);
+    if ($fail != '')
+    {
+       notifyError($fail, "register.php");
+       $corrMsg ="<it>Internal: failed access to registration record of ".$userID."</it>";
+    }
+  }
+  dbClose($db_conn);
 
-if ($wasUpdated)
-{
-   // post change interface
-   if (isset($_POST["submit"]))
-   {
-     $nextURL = "payRegFee.php";
-   }
-   else
-   {
-     $nextURL = "index.php";
-   }
-   sendUpdateEmail($email, $registrant);
-   getNextPage($nextURL);
-}
-else
-{
-   // flying information form
-   // $corrMsg has HTML content
-   // $registrant has POST content
-   startHead("Nationals Flying Information");
-   flyingFormHeader();
-   startContent("onload='checkOwnerPilot()'");
-   echo "<h1>Flying Information</h1>";
-   verificationHeader("For");
-   if ($corrMsg != '')
-   {
-      echo '<ul class="error">'.$corrMsg.'</ul>';
-   }
-   flyingForm($registrant, "flying.php");
-   echo '<div class="returnButton"><a href="index.php">Return without saving</a></div>';
-   endContent();
-}
+  if ($wasUpdated)
+  {
+     // post change interface
+     if (isset($_POST["submit"]))
+     {
+       $nextURL = "payRegFee.php";
+     }
+     else
+     {
+       $nextURL = "index.php";
+     }
+     sendUpdateEmail($email, $registrant);
+     getNextPage($nextURL);
+  }
+  else
+  {
+     // flying information form
+     // $corrMsg has HTML content
+     // $registrant has POST content
+     startHead("Nationals Flying Information");
+     flyingFormHeader();
+     startContent("onload='checkOwnerPilot()'");
+     echo "<h1>Flying Information</h1>";
+     verificationHeader("For");
+     if ($corrMsg != '')
+     {
+        echo '<ul class="error">'.$corrMsg.'</ul>';
+     }
+     flyingForm($registrant, "flying.php");
+     echo '<div class="returnButton"><a href="index.php">Return without saving</a></div>';
+     endContent();
+  }
 }
 
 if (isRegOpen())
